ce13db47900b53b6e84ca7ddb4e2e63162f07f95
tipusahil <tipusahil.ctg@gmail.com>
2025-03-07 16:35:03 +0600

    second commit ol localStorage

--- ES11_replaceAll.js
+++ ES11_replaceAll.js
@@ -1,3 +1,8 @@
-let langu = 'i love 1 javascript, 2 javascipt';
-langu.replace('hablu');
-console.log(lnagu)
+let langu = 'i love 1 javascript, 2 javascript';
+let afterReplace = langu.replace('javascript' ,"js");//protom (javascript)word ta (js) diye replace korse,ar jodi replace all ditm taile oi string e ja ase sob replace hoye jeto jmn:
+console.log(afterReplace)
+
+// ------------------2
+let x = 'ami tipu tipu tipu sahil '
+let replaceAll_1 = x.replaceAll("tipu" , "mustafa")
+console.log(replaceAll_1)//output: ami mustafa mustafa mustafa sahil //tipu name ja ase sob mustafa name diye replace hoye gece
--- intro/intro.js
+++ intro/intro.js
@@ -1,10 +1,13 @@
 /* 
+// (localStorage) ta browser er api so eta use korte  caile (ekta html file create kroe sei file er sate js file ta )add atta lagai then (localStorage) ta use korte hbe.jeehto amr ekn ei js file ta html file er sate linkup kora ase & html file to browser e cole tai (localStorage)api ta use kora jabe.
+jmn: 22
+
 
 // kuno object browser er localStorage e set korte caile must be (JSON.stringify(object)) ta convert korte hbe, hlei sei object ta browser e set hbe. & sei browser e set kora obj ta browser theke access korte caile seta access korar pore (JSON.parse()) etar maddome stringify object theke normal object e covnert korte hbe ,thlei kaj korbe, 
 
 ##eta korar karon hocce 
 
-
+@ localStorage e object rakte ( JSON.stringify(objectName)  ) er maddome string e convert kore rakte hbe, & jodi sei localStorage er object ta abr use korte cai kinba sei object er kuno value/data jaihokna keno use korte caile seta k (JSON.parse()) er maddome objectstring theke object e covert korte hbe jate sei object key/value/entries sob ja mon cai access kora jai.
 */
 
 const object1 = {name:'tipu' , age:22};
@@ -15,3 +18,10 @@ console.log(after_converted_JSON_stringify);// output: {"name":"tipu","age":22}
 
 
 // again stringify to json parse er maoddome object e convert krte caile:
+
+
+// 22
+
+localStorage.setItem("add1" , "add1 er value ami ")
+const getdata = localStorage.getItem("add1");
+console.log(getdata)
