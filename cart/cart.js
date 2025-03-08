1349f0ce0f9eacd1f46c3c1e25f2f110269acd97
tipusahil <tipusahil.ctg@gmail.com>
2025-03-08 12:45:29 +0600

    cart file createed & cart add to js

---
+++ cart/cart.js
@@ -0,0 +1,15 @@
+const addcart = () => {
+  const product_input = document.getElementById('product');
+  const product_quantity_input = document.getElementById('product-quantity');
+
+  const product = product_input.value;
+  const product_quantity = product_quantity_input.value;
+product.value ='';
+product_quantity.value ='';
+  console.log(product,product_quantity)
+
+
+
+
+
+}
